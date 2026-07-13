c4cfeaf1ef9e0702c19d5ce50d6e7f24f0939fb9
Admin PMR <pmrsmanel@gmail.com>
2026-07-13 22:30:47 +0800

    Fix BrowserRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles/globals.css";
-
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      basename="/pmr-registration-system"
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
