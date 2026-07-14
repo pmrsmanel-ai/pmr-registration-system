62c245564eab24c0abee7796f691fa6a1540180d
Admin PMR <pmrsmanel@gmail.com>
2026-07-14 09:09:16 +0800

    Fix custom domain routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename="/pmr-registration-system"
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
